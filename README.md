7cbbc4bc3e02e08d702d58a190ea65dbfd0cb0c4
stevepraski <stevenpraski@gmail.com>
2018-12-18 20:32:31 -0800

    Refactor eco docker server

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # edc-box
 
-### Tested on:
+## WARNING
+Work in progress.  Use at your own risk
+
+### HOST Audio part of EDC tested on:
  * Ubuntu 16.04
  * MacOS High Sierra 10.13.6
--- eco-docker/bootstrap-eco-server.sh
+++ eco-docker/bootstrap-eco-server.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 set -e
-
-apt-get install -y software-properties-common curl
 apt-add-repository -y --update ppa:ansible/ansible
+apt-get update
+apt-get install -y software-properties-common curl
 apt-get install -y ansible
 mkdir -p /opt/eco-docker-bootstrap
 cd /opt/eco-docker-bootstrap
-curl -L https://api.github.com/repos/stevepraski/edc-box/tarball > edc-box.tar.gz
-tar -xzf edc-box.tar.gz --strip-components=1
+curl -L https://api.github.com/repos/stevepraski/edc-box/tarball | tar -xzf - --strip-components=1
 cat << EOF > eco-docker-playbook.yml
 ---
 - hosts: 127.0.0.1
